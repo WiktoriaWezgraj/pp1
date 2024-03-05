719c9ac8fab7821b1cde28a04acae6731cd1af8b
WiktoriaWezgraj <wiktoriawezgraj1@gmail.com>
2024-03-05 19:39:10 +0100

    update

---
+++ 04-Subroutines/4.12.py
@@ -0,0 +1,20 @@
+# task a
+max_number = max(7,5,6,3,8,2)
+print("Max number is",max_number)
+
+# task b
+decimal = 304
+binary = bin(decimal)
+print(f"Binary from {decimal} is",binary)
+
+# task c
+hexadecimal = hex(decimal)
+print("Hexadecimal is",hexadecimal)
+
+# task d
+unicode = ord('$')
+print("Unicode of the $ sign is", unicode)
+
+# task e
+absolute_value = abs(-17)
+print("Absolute value of -17 is", absolute_value)
---
+++ 04-Subroutines/4.13.py
@@ -0,0 +1,7 @@
+import math as m
+
+# task a
+natural_log = m.log(5)
+print("Natural log is:",natural_log)
+
+# task b
